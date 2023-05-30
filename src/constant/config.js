d2433b8d9921d186aa004634d9423c5f722ab6ab
venusgalstar <venusgalstar@gmail.com>
2023-05-31 03:40:41 +0900

    added token staking info

--- src/constant/config.js
+++ src/constant/config.js
@@ -1,7 +1,7 @@
 import nftAbi from './nft.json';
 import nftStakeAbi from './nftStake.json';
 import tokenAbi from './token.json';
-// import tokenStakeAbi from './tokenStake.json';
+import tokenStakeAbi from './tokenStake.json';
 
 var config = {
     nftAddress: "0xAEAed6335EDaBbAa074f6C82910c36Bf7FdA9109",
@@ -10,6 +10,8 @@ var config = {
     tokenAbi: tokenAbi,
     nftStakeAddress: "0xeD698F7a543172a18655fF9998587e98dA417A7F",
     nftStakeAbi: nftStakeAbi,
+    tokenStakeAddress: "0x68F4aeC1c852E13cCED55bB8Bd970b287f5A155c",
+    tokenStakeAbi: tokenStakeAbi,
     RPC_URL: "https://data-seed-prebsc-2-s2.binance.org:8545",
 };
 
