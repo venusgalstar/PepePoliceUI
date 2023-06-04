26f5baf807030979815c2d238c4a32ea0dbd4c00
= <=>
2023-06-04 15:27:48 +0200

    update nft abi and nft staking abi and addresses

--- src/constant/config.js
+++ src/constant/config.js
@@ -4,11 +4,11 @@ import tokenAbi from './token.json';
 import tokenStakeAbi from './tokenStake.json';
 
 var config = {
-    nftAddress: "0xAEAed6335EDaBbAa074f6C82910c36Bf7FdA9109",
+    nftAddress: "0xd60170a1fA03F27e41b2aBb90Cc139e5F6397173",
     nftAbi: nftAbi,
     tokenAddress: "0xFD2a3ceffE78Af023472012cCbc7282FF7B45f6F",
     tokenAbi: tokenAbi,
-    nftStakeAddress: "0xeD698F7a543172a18655fF9998587e98dA417A7F",
+    nftStakeAddress: "0xd6597ffbE3f655023cFb0037A2e53DC98d31CB7D",
     nftStakeAbi: nftStakeAbi,
     tokenStakeAddress: "0x68F4aeC1c852E13cCED55bB8Bd970b287f5A155c",
     tokenStakeAbi: tokenStakeAbi,
